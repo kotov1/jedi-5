301d9d0b1013c67ad144be554e131eb187a54190
kotov1 <kotov0991@gmail.com>
2017-01-22 10:59:00 +0300

    header adding

--- app/js/common.js
+++ app/js/common.js
@@ -3,3 +3,11 @@ $(function() {
 	// Custom JS
 
 });
+
+
+// SVG Fallback
+if(!Modernizr.svg) {
+  $('img[src*="svg"]').attr('src', function() {
+    return $(this).attr('src').replace('.svg', '.png');
+  });
+};
