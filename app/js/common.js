155816d2c79fb927bd31e083538af5686b7c677c
kotov1 <kotov0991@gmail.com>
2017-02-01 21:03:25 +0300

    finished header

--- app/js/common.js
+++ app/js/common.js
@@ -1,13 +1,20 @@
-$(function() {
+$(document).ready(function() {
 
-	// Custom JS
+	$(".toggle-menu").click(function() {
+		$(this).toggleClass("on");
+		$(".main-menu").slideToggle();
+		return false;
+	});
+
+	
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	
 
 });
-
-
-// SVG Fallback
-if(!Modernizr.svg) {
-  $('img[src*="svg"]').attr('src', function() {
-    return $(this).attr('src').replace('.svg', '.png');
-  });
-};
