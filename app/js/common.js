d5917574ae25547436ef8d536ccc1e8c644e8e6e
kotov1 <kotov0991@gmail.com>
2017-05-21 22:47:55 +0300

    Email feedback

--- app/js/common.js
+++ app/js/common.js
@@ -47,6 +47,25 @@ $(document).ready(function() {
 		});
 	};
 
+
+	// E-mail Ajax Send
+	$(".mail").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "../../mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+
 	
 
 });
